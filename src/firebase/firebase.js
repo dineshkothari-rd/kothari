38ef309315b977927d05fb92716f882df5acb424
Dinesh Choudhary <dineshkothari2021@gmail.com>
2026-04-12 12:22:33 +0530

    add env variables for firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqJycrKpeIxEmZUkUCjO6PN8RZb9GoLhM",
-  authDomain: "kothari-pg.firebaseapp.com",
-  projectId: "kothari-pg",
-  storageBucket: "kothari-pg.firebasestorage.app",
-  messagingSenderId: "481768124087",
-  appId: "1:481768124087:web:49df4bb5ac5905540fa732",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
